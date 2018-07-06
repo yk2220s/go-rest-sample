8a25dad085d8ba69fa826153be3b5e3222ed63c3
Yuichi KIshimoto <yk2220s@gmail.com>
2018-07-06 15:27:19 +0900

    [Modify] make controller.

---
+++ api/controller/user.go
@@ -0,0 +1,12 @@
+package controller
+
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+// UserList fetch List of users
+func UserList(c *gin.Context) {
+    users := [2]string{"user1", "user2"}
+    c.JSON(http.StatusOK, gin.H{"users": users})
+}
--- main.go
+++ main.go
@@ -1,57 +1,33 @@
 package main
 
 import (
+    "net/http"
 	"github.com/gin-gonic/gin"
+    "github.com/yk2220s/go-rest-sample/api/controller"
 )
-
-var DB = make(map[string]string)
 
 func setupRouter() *gin.Engine {
 	// Disable Console Color
 	// gin.DisableConsoleColor()
 	r := gin.Default()
 
-	// Ping test
+    // Ping test
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "ok")
+	})
+
+    // Ping test
 	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
+		c.JSON(http.StatusOK, gin.H{"ping": "pong"})
 	})
 
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := DB[user]
-		if ok {
-			c.JSON(200, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(200, gin.H{"user": user, "status": "no value"})
-		}
-	})
+    // User resource.
+    r.GET("/users", controller.UserList)
 
-	// Authorized group (uses gin.BasicAuth() middleware)
-	// Same than:
-	// authorized := r.Group("/")
-	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	  "foo":  "bar",
-	//	  "manu": "123",
-	//}))
-	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
-		"foo":  "bar", // user:foo password:bar
-		"manu": "123", // user:manu password:123
-	}))
-
-	authorized.POST("admin", func(c *gin.Context) {
-		user := c.MustGet(gin.AuthUserKey).(string)
-
-		// Parse JSON
-		var json struct {
-			Value string `json:"value" binding:"required"`
-		}
-
-		if c.Bind(&json) == nil {
-			DB[user] = json.Value
-			c.JSON(200, gin.H{"status": "ok"})
-		}
-	})
+    r.GET("/users/:id", func(c *gin.Context) {
+        id := c.Params.ByName("id")
+        c.JSON(http.StatusOK, gin.H{"userId": id})
+    })
 
 	return r
 }
