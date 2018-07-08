46997da828392f7d13851999550c051a4752c7d9
Yuichi KIshimoto <yk2220s@gmail.com>
2018-07-09 00:35:03 +0900

    [Fix] Fix model mapping.

--- api/model/user.go
+++ api/model/user.go
@@ -6,10 +6,10 @@ import (
 
 // User is Entity of user
 type User struct {
-	UserID    int64     `gorm:"primary_key;not null"`
-	Name      string    `gorm:"size:255;not null"`
-	Email     string    `gorm:"size:255;not null"`
-	CreatedAt time.Time `gorm:"not null"`
-	UpdatedAt time.Time `gorm:"not null"`
-	DeletedAt *time.Time
+	UserID    int64      `gorm:"primary_key;not null" json:"userId"`
+	Name      string     `gorm:"size:255;not null" json:"name"`
+	Email     string     `gorm:"size:255;not null" json:"email"`
+	CreatedAt time.Time  `gorm:"not null" json:"createdAt"`
+	UpdatedAt time.Time  `gorm:"not null" json:"updatedAt"`
+	DeletedAt *time.Time `json:"deletedAt"`
 }
