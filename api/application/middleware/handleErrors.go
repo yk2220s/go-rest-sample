ddad1da8d962faa1d366f0fc5252e3d420e7adac
Yuichi KIshimoto <yk2220s@gmail.com>
2018-07-19 23:25:22 +0900

    [Fix] Fix middleware.

--- api/application/middleware/handleErrors.go
+++ api/application/middleware/handleErrors.go
@@ -1,12 +1,18 @@
 package middleware
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 // HandleErrors handle error to prepare error response
 func HandleErrors(c *gin.Context) {
 	c.Next()
 
-	if len(c.Errors) != 0 {
-		//c.JSON(-1, c.Errors) // -1 == not override the current error code
+	if len(c.Errors) == 0 {
+		return
 	}
+
+	lastError := c.Errors.Last()
+
+	c.IndentedJSON(-1, lastError)
 }
