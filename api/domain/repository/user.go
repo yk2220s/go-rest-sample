38c5585e12110b369039bc61da52ef3c95b89e75
Yuichi KIshimoto <yk2220s@gmail.com>
2018-07-19 23:24:51 +0900

    [Fix] Refactor Repository.

--- api/domain/repository/user.go
+++ api/domain/repository/user.go
@@ -2,17 +2,17 @@ package repository
 
 import (
 	"github.com/yk2220s/go-rest-sample/api/database"
-	"github.com/yk2220s/go-rest-sample/api/domain/domainerror"
+	"github.com/yk2220s/go-rest-sample/api/domain/exception"
 	"github.com/yk2220s/go-rest-sample/api/domain/model"
 )
 
 // UserRepository access UserEntity
 type UserRepository interface {
 	List(page int) []*model.User
-	GetByID(userID int) (*model.User, domainerror.DomainError)
-	Store(user *model.User) (*model.User, error)
-	Update(user *model.User) (*model.User, error)
-	Delete(userID int) (bool, error)
+	GetByID(userID int) (*model.User, exception.DomainError)
+	Store(user *model.User) (*model.User, exception.DomainError)
+	Update(user *model.User) (*model.User, exception.DomainError)
+	Delete(userID int) (bool, exception.DomainError)
 }
 
 // UserRepositoryFactory make UserRepository
@@ -45,55 +45,57 @@ func (repo *UserRepositoryImpl) List(page int) []*model.User {
 }
 
 // GetByID fetch user by id
-func (repo *UserRepositoryImpl) GetByID(userID int) (*model.User, domainerror.DomainError) {
+func (repo *UserRepositoryImpl) GetByID(userID int) (*model.User, exception.DomainError) {
 	db := database.Open()
 	defer db.Close()
 
 	var user model.User
 
 	if db.First(&user, userID).RecordNotFound() {
-		return nil, domainerror.NewNotFound()
+		return nil, exception.NewNotFound()
 	}
 
 	return &user, nil
 }
 
 // Store save new user model.
-func (repo *UserRepositoryImpl) Store(user *model.User) (*model.User, error) {
+func (repo *UserRepositoryImpl) Store(user *model.User) (*model.User, exception.DomainError) {
 	db := database.Open()
 	defer db.Close()
 
 	if err := db.Create(&user).Error; err != nil {
-		return nil, err
+		return nil, exception.NewDatabaseError(err.Error())
 	}
 
 	return user, nil
 }
 
 // Update update user model
-func (repo *UserRepositoryImpl) Update(user *model.User) (*model.User, error) {
+func (repo *UserRepositoryImpl) Update(user *model.User) (*model.User, exception.DomainError) {
 	db := database.Open()
 	defer db.Close()
 
 	if err := db.Save(&user).Error; err != nil {
-		return nil, err
+		return nil, exception.NewDatabaseError(err.Error())
 	}
 
 	return user, nil
 }
 
 // Delete delete user model
-func (repo *UserRepositoryImpl) Delete(userID int) (bool, error) {
+func (repo *UserRepositoryImpl) Delete(userID int) (bool, exception.DomainError) {
 	db := database.Open()
 	defer db.Close()
 
 	var user model.User
 
 	if db.First(&user, userID).RecordNotFound() {
-		return false, domainerror.NewNotFound()
+		return false, exception.NewNotFound()
 	}
 
-	db.Delete(&user)
+	if err := db.Delete(&user).Error; err != nil {
+		return false, exception.NewDatabaseError(err.Error())
+	}
 
 	return true, nil
 }
